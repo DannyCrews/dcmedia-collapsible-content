4354d9269af91370122bb8ab0560fb480fdc4263
dannycrews <dancrews@me.com>
2017-04-16 10:21:10 -0400

    Built HTML view files

--- src/shortcode/shortcodes.php
+++ src/shortcode/shortcodes.php
@@ -9,3 +9,6 @@
  * Time: 2:49 PM
  * @license GNU General Public License 2.0+
  */
+//[qa question="What question would you like to ask me?" show_icon="" hide_icon=""]this is the answer[/qa]
+
+//[teaser visible_message="What question would you like to ask me?" show_icon="" hide_icon=""]this is the answer[/teaser]
